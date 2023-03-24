8d9a96983ae5eb201066f2db3a3016560e61f20a
Ajith101 <ajithpktd@gmail.com>
2023-03-24 21:39:05 +0530

    added some change

--- models/models.js
+++ models/models.js
@@ -4,6 +4,7 @@ const TodosScheema = new mongoose.Schema({
   todo: String,
   like: Boolean,
   time: String,
+  edited: { type: String, default: null },
 });
 
 const Todos = mongoose.model("todo", TodosScheema);
